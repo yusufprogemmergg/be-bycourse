a4ca45ab8c4dd5d6283edee9a02f759a8c92b75d
Wreda Prasetiyo <yusufdwiprasetiyo@gmail.com>
2025-05-24 21:45:55 +0700

    upadate createprofile4

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.DATABASE_URL!,
+  process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // HARUS pakai service role untuk upload dari backend
 );
 
