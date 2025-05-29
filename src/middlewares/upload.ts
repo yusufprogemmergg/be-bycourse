78946eb22aa2c01db9ccc5f24017d51a136ea420
Wreda Prasetiyo <yusufdwiprasetiyo@gmail.com>
2025-05-29 19:10:42 +0700

    add module lesson4

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,14 +1,19 @@
-// middleware/upload.ts
 import multer from 'multer';
 import path from 'path';
 import os from 'os';
 
 const storage = multer.diskStorage({
-  destination: os.tmpdir(), // tempat aman sementara
+  destination: os.tmpdir(), // folder sementara di OS
   filename: (req, file, cb) => {
     const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, unique + path.extname(file.originalname));
-  }
+  },
 });
 
-export const upload = multer({ storage });
+// ⚠️ Tambahkan batas ukuran file, misal max 100MB (supabase biasanya max 50MB)
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1000 * 1024 * 1024, // 100MB
+  },
+});
