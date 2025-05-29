8ff2660e0deff7af9ef274f2710f699cfdaae200
Wreda Prasetiyo <yusufdwiprasetiyo@gmail.com>
2025-05-29 19:14:47 +0700

    add module lesson5

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req: Request, res: Response) => {
