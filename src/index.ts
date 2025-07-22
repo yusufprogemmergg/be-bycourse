ed96725b08a830876665da37682482f4fb14c76a
Wreda Prasetiyo <yusufdwiprasetiyo@gmail.com>
2025-07-23 06:53:33 +0700

    addmagiclink

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import app from '../src/app';
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import orderRoutes from './routes/orderRoutes'; // sesuaikan dengan lokasi route
+
+app.use(cors({
+    origin: ['https://fe-bycourse.vercel.app'],
+    credentials: true, // jika pakai cookie
+}));
 
 
 dotenv.config();
